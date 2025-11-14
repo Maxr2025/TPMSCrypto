273497a8c0b878a485e0d808566b6e47fe25ccec
max <maxr2025@gmail.com>
2025-11-14 17:21:22 -0500

    end2end updates for real hardware

--- SDR/end2end_test/end2end.py
+++ SDR/end2end_test/end2end.py
@@ -10,10 +10,13 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
+from PyQt5 import QtCore
+from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import digital
-from gnuradio import gr
+from gnuradio import filter
 from gnuradio.filter import firdes
+from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
@@ -21,6 +24,7 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import soapy
 import sip
 
 
@@ -60,59 +64,95 @@ class end2end(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.tx_gain = tx_gain = 0.9
+        self.sync_word = sync_word = b'\x2D\xD4'
+        self.squel = squel = -5
         self.sps = sps = 2
-        self.samp_rate = samp_rate = 32000
+        self.samp_rate = samp_rate = 386e3
+        self.rx_gain = rx_gain = 1.4
+        self.preamble = preamble = b'\x55' * 100
+        self.ppm = ppm = 11
+        self.payload = payload = b'\xDE\xAD\xBE\xEF'*100
+        self.header = header = b'\x01\x90\x01\x90'
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-            1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            samp_rate, #bw
-            "", #name
-            1,
-            None # parent
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis((-140), 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(False)
-        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
+        self._tx_gain_range = qtgui.Range(0, 1, 0.01, 0.9, 200)
+        self._tx_gain_win = qtgui.RangeWidget(self._tx_gain_range, self.set_tx_gain, "'tx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._tx_gain_win)
+        self._squel_range = qtgui.Range(-100, 100, 1, -5, 200)
+        self._squel_win = qtgui.RangeWidget(self._squel_range, self.set_squel, "'squel'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._squel_win)
+        self._rx_gain_range = qtgui.Range(0, 100, 0.1, 1.4, 200)
+        self._rx_gain_win = qtgui.RangeWidget(self._rx_gain_range, self.set_rx_gain, "'rx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._rx_gain_win)
+        self._ppm_range = qtgui.Range(-100, 100, 1, 11, 200)
+        self._ppm_win = qtgui.RangeWidget(self._ppm_range, self.set_ppm, "'ppm'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._ppm_win)
+        self.soapy_rtlsdr_source_0 = None
+        dev = 'driver=rtlsdr'
+        stream_args = 'bufflen=16384'
+        tune_args = ['']
+        settings = ['']
 
+        def _set_soapy_rtlsdr_source_0_gain_mode(channel, agc):
+            self.soapy_rtlsdr_source_0.set_gain_mode(channel, agc)
+            if not agc:
+                  self.soapy_rtlsdr_source_0.set_gain(channel, self._soapy_rtlsdr_source_0_gain_value)
+        self.set_soapy_rtlsdr_source_0_gain_mode = _set_soapy_rtlsdr_source_0_gain_mode
 
+        def _set_soapy_rtlsdr_source_0_gain(channel, name, gain):
+            self._soapy_rtlsdr_source_0_gain_value = gain
+            if not self.soapy_rtlsdr_source_0.get_gain_mode(channel):
+                self.soapy_rtlsdr_source_0.set_gain(channel, gain)
+        self.set_soapy_rtlsdr_source_0_gain = _set_soapy_rtlsdr_source_0_gain
 
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
+        def _set_soapy_rtlsdr_source_0_bias(bias):
+            if 'biastee' in self._soapy_rtlsdr_source_0_setting_keys:
+                self.soapy_rtlsdr_source_0.write_setting('biastee', bias)
+        self.set_soapy_rtlsdr_source_0_bias = _set_soapy_rtlsdr_source_0_bias
 
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+        self.soapy_rtlsdr_source_0 = soapy.source(dev, "fc32", 1, '',
+                                  stream_args, tune_args, settings)
 
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self._soapy_rtlsdr_source_0_setting_keys = [a.key for a in self.soapy_rtlsdr_source_0.get_setting_info()]
+
+        self.soapy_rtlsdr_source_0.set_sample_rate(0, 1.8e6)
+        self.soapy_rtlsdr_source_0.set_frequency(0, 464e6)
+        self.soapy_rtlsdr_source_0.set_frequency_correction(0, ppm)
+        self.set_soapy_rtlsdr_source_0_bias(bool(False))
+        self._soapy_rtlsdr_source_0_gain_value = 20
+        self.set_soapy_rtlsdr_source_0_gain_mode(0, bool(False))
+        self.set_soapy_rtlsdr_source_0_gain(0, 'TUNER', 20)
+        self.soapy_hackrf_sink_0 = None
+        dev = 'driver=hackrf'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+
+        self.soapy_hackrf_sink_0 = soapy.sink(dev, "fc32", 1, '',
+                                  stream_args, tune_args, settings)
+        self.soapy_hackrf_sink_0.set_sample_rate(0, 10e6)
+        self.soapy_hackrf_sink_0.set_bandwidth(0, 0)
+        self.soapy_hackrf_sink_0.set_frequency(0, 464e6)
+        self.soapy_hackrf_sink_0.set_gain(0, 'AMP', False)
+        self.soapy_hackrf_sink_0.set_gain(0, 'VGA', min(max(16, 0.0), 47.0))
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_ccc(
+                interpolation=int(samp_rate),
+                decimation=int(1.8e6),
+                taps=[],
+                fractional_bw=0)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=int(10e6),
+                decimation=int(samp_rate),
+                taps=[],
+                fractional_bw=0)
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
             1024, #size
             "", #name
-            1, #number of inputs
+            2, #number of inputs
             None # parent
         )
         self.qtgui_const_sink_x_0.set_update_time(0.10)
@@ -137,7 +177,7 @@ class end2end(gr.top_block, Qt.QWidget):
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
 
-        for i in range(1):
+        for i in range(2):
             if len(labels[i]) == 0:
                 self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -168,24 +208,33 @@ class end2end(gr.top_block, Qt.QWidget):
             log=False)
         self.digital_correlate_access_code_xx_ts_0 = digital.correlate_access_code_bb_ts('0010110111010100',
           0, '')
-        self.blocks_vector_source_x_0 = blocks.vector_source_b(list(b'\x2D\xD4'), True, 1, [])
+        self.blocks_vector_source_x_0 = blocks.vector_source_b(list(preamble + sync_word + header + payload), True, 1, [])
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(8)
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_cc(tx_gain)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(rx_gain)
         self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, 'recieved_data.bin', False)
         self.blocks_file_sink_0.set_unbuffered(True)
+        self.analog_simple_squelch_cc_0 = analog.simple_squelch_cc(squel, 1)
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_simple_squelch_cc_0, 0), (self.digital_gfsk_demod_0, 0))
+        self.connect((self.analog_simple_squelch_cc_0, 0), (self.qtgui_const_sink_x_0, 1))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.analog_simple_squelch_cc_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.digital_gfsk_mod_0, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.digital_correlate_access_code_xx_ts_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
         self.connect((self.digital_gfsk_demod_0, 0), (self.digital_correlate_access_code_xx_ts_0, 0))
-        self.connect((self.digital_gfsk_mod_0, 0), (self.digital_gfsk_demod_0, 0))
-        self.connect((self.digital_gfsk_mod_0, 0), (self.qtgui_const_sink_x_0, 0))
-        self.connect((self.digital_gfsk_mod_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.digital_gfsk_mod_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.soapy_hackrf_sink_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.soapy_rtlsdr_source_0, 0), (self.rational_resampler_xxx_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -195,6 +244,27 @@ class end2end(gr.top_block, Qt.QWidget):
         self.wait()
 
         event.accept()
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+        self.blocks_multiply_const_vxx_0_0.set_k(self.tx_gain)
+
+    def get_sync_word(self):
+        return self.sync_word
+
+    def set_sync_word(self, sync_word):
+        self.sync_word = sync_word
+        self.blocks_vector_source_x_0.set_data(list(self.preamble + self.sync_word + self.header + self.payload), [])
+
+    def get_squel(self):
+        return self.squel
+
+    def set_squel(self, squel):
+        self.squel = squel
+        self.analog_simple_squelch_cc_0.set_threshold(self.squel)
 
     def get_sps(self):
         return self.sps
@@ -208,7 +278,41 @@ class end2end(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+
+    def get_rx_gain(self):
+        return self.rx_gain
+
+    def set_rx_gain(self, rx_gain):
+        self.rx_gain = rx_gain
+        self.blocks_multiply_const_vxx_0.set_k(self.rx_gain)
+
+    def get_preamble(self):
+        return self.preamble
+
+    def set_preamble(self, preamble):
+        self.preamble = preamble
+        self.blocks_vector_source_x_0.set_data(list(self.preamble + self.sync_word + self.header + self.payload), [])
+
+    def get_ppm(self):
+        return self.ppm
+
+    def set_ppm(self, ppm):
+        self.ppm = ppm
+        self.soapy_rtlsdr_source_0.set_frequency_correction(0, self.ppm)
+
+    def get_payload(self):
+        return self.payload
+
+    def set_payload(self, payload):
+        self.payload = payload
+        self.blocks_vector_source_x_0.set_data(list(self.preamble + self.sync_word + self.header + self.payload), [])
+
+    def get_header(self):
+        return self.header
+
+    def set_header(self, header):
+        self.header = header
+        self.blocks_vector_source_x_0.set_data(list(self.preamble + self.sync_word + self.header + self.payload), [])
 
 
 
--- SDR/end2end_test/readme.md
+++ SDR/end2end_test/readme.md
@@ -1,1 +1,1 @@
-end2end_test is for simulating the RF chain in software for proof of concept and debugging
+end2end_test is for simulating the RF chain for proof of concept and debugging prior to functionalization
